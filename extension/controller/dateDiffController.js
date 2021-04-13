2a0ad7c0f3b54c42946db4d13bfe1dbae6724c4e
jonathan <agundez.l.jonathan@gmail.com>
2021-04-13 17:56:57 +0200

    Provisional

--- extension/controller/dateDiffController.js
+++ extension/controller/dateDiffController.js
@@ -19,96 +19,96 @@ module.exports = function (nodecg) {
 	var r = nodecg.readReplicant('DateDiffInstances');
 	nodecg.log.debug(r)
 
-	for (var i = 0; i < r.length; r++) {
-		var ci = r[i]
-	// 	nodecg.log.debug(r[i])
-		var co = nodecg.readReplicant(`datediff-${ci.name}`)
-		if (!co || !co.name) co = ci;
+	// for (var i = 0; i < r.length; r++) {
+	// 	var ci = r[i]
+	// // 	nodecg.log.debug(r[i])
+	// 	var co = nodecg.readReplicant(`datediff-${ci.name}`)
+	// 	if (!co || !co.name) co = ci;
 
-		var datediff = new DateDiff(co.name, co.elapsed, co.end_datetime, co.compute, co.format)
+	// 	var datediff = new DateDiff(co.name, co.elapsed, co.end_datetime, co.compute, co.format)
 				
 	// 	if (!datediff) {
 	// 		nodecg.Replicant('datediff-'+i.toString(), {defaultValue: datediff, persistent:true})
 	// 	}
 
-		startWatch(datediff)
-	}
-	
-	
-	
-
-
-
-
-	// initializeReplicants();
-
-	// const app = nodecg.Router();
-    // app.post('/timetools/new-datediff', (req, res) => {
-    //     newDateDiff(req.body.min)
-    //     res.send();
-    // });
-	// app.post('/timetools/delete-datediff', (req, res) => {
-    //     nodecg.Replicant('DateDiffInstances').value.splice(req.data, 1)
-    //     res.send();
-    // });
-    // nodecg.mount(app);
-
-	// function newDateDiff(i) {
-	// 	var settings = default_config_datediff
-	// 	settings.name = `datediff-${i}`;
-		
-	// 	if (!Array.isArray(nodecg.readReplicant('DateDiffInstances'))) {
-	// 		nodecg.Replicant('DateDiffInstances').value = []
-	// 	}
-		
-	// 	var arr =[];
-	// 	for (var c = 0; c < nodecg.readReplicant('DateDiffInstances').length; c++) {
-	// 		arr.push(nodecg.readReplicant('DateDiffInstances')[c])
-	// 	}
-	// 	arr.push(settings)
-	// 	nodecg.log.debug(arr);
-
-	// 	nodecg.Replicant('DateDiffInstances').value = arr;
-	// 	// nodecg.Replicant('DateDiffInstances').value = arr; 
-		
-	// 	initializeReplicants(settings)
+	// 	startWatch(datediff)
 	// }
+	
+	
+	
+
+
+
+
+	initializeReplicants();
+
+	const app = nodecg.Router();
+    app.post('/timetools/new-datediff', (req, res) => {
+        newDateDiff(req.body.min)
+        res.send();
+    });
+	app.post('/timetools/delete-datediff', (req, res) => {
+        nodecg.Replicant('DateDiffInstances').value.splice(req.data, 1)
+        res.send();
+    });
+    nodecg.mount(app);
+
+	function newDateDiff(i) {
+		var settings = default_config_datediff
+		settings.name = `datediff-${i}`;
+		
+		if (!Array.isArray(nodecg.readReplicant('DateDiffInstances'))) {
+			nodecg.Replicant('DateDiffInstances').value = []
+		}
+		
+		var arr =[];
+		for (var c = 0; c < nodecg.readReplicant('DateDiffInstances').length; c++) {
+			arr.push(nodecg.readReplicant('DateDiffInstances')[c])
+		}
+		arr.push(settings)
+		nodecg.log.debug(arr);
+
+		nodecg.Replicant('DateDiffInstances').value = arr;
+		// nodecg.Replicant('DateDiffInstances').value = arr; 
+		
+		initializeReplicants(settings)
+	}
 
     // Create/resume classes
-	// function initializeReplicants(default_conf) {
-	// 	if (!default_conf) {
-	// 		default_conf = default_config_datediff
-	// 	}
+	function initializeReplicants(default_conf) {
+		if (!default_conf) {
+			default_conf = default_config_datediff
+		}
 		
-	// 	var datediff = null;
-	// 	if (!Array.isArray(nodecg.readReplicant('DateDiffInstances'))) {
-	// 		nodecg.Replicant('DateDiffInstances').value = default_conf_arr
-	// 	}
-	// 	var DateDiffInstances = nodecg.Replicant('DateDiffInstances').value;
-	// 	for (var i = 0; i < nodecg.Replicant('DateDiffInstances').value.length; i++) {
-	// 		var ci = DateDiffInstances[i]
-	// 		var co = null;
-	// 		if (nodecg.Replicant(`datediff-${ci.name}`)) {
-	// 			co = nodecg.Replicant(`datediff-${ci.name}`).value
-	// 		}
+		var datediff = null;
+		if (!Array.isArray(nodecg.readReplicant('DateDiffInstances'))) {
+			nodecg.Replicant('DateDiffInstances').value = default_conf_arr
+		}
+		var DateDiffInstances = nodecg.Replicant('DateDiffInstances').value;
+		for (var i = 0; i < nodecg.Replicant('DateDiffInstances').value.length; i++) {
+			var ci = DateDiffInstances[i]
+			var co = null;
+			if (nodecg.Replicant(`datediff-${ci.name}`)) {
+				co = nodecg.Replicant(`datediff-${ci.name}`).value
+			}
 			
-	// 		if (!co || !co.name) co = ci;
+			if (!co || !co.name) co = ci;
 			
-	// 		datediff = new DateDiff(co.name, co.elapsed, co.end_datetime, co.compute, co.format)
+			datediff = new DateDiff(co.name, co.elapsed, co.end_datetime, co.compute, co.format)
 			
-	// 		if (!datediff || !datediff.name) {
-	// 			nodecg.Replicant('datediff-'+i.toString(), {defaultValue: datediff, persistent:true})
-	// 		}
+			if (!datediff || !datediff.name) {
+				nodecg.Replicant('datediff-'+i.toString(), {defaultValue: datediff, persistent:true})
+			}
 				
-	// 		var val = nodecg.Replicant('datediff-'+i.toString()).value
+			var val = nodecg.Replicant('datediff-'+i.toString()).value
 			
-	// 		if (!val || val == '') {
-	// 			nodecg.Replicant('datediff-'+i.toString()).value = datediff;
-	// 		}
+			if (!val || val == '') {
+				nodecg.Replicant('datediff-'+i.toString()).value = datediff;
+			}
 			
-	// 		startWatch(datediff)
-	// 	}
-	// }
+			startWatch(datediff)
+		}
+	}
 	
 
 	function startWatch(obj) {
